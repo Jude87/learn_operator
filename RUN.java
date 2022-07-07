603fffe9749f797d5efa89ceb23788f4e33de69f
Jude87 <107044598+Jude87@users.noreply.github.com>
2022-07-07 17:40:00 +0900

    Add files via upload

---
+++ RUN.java
@@ -0,0 +1,13 @@
+package example.operator;
+
+public class RUN {
+
+	public static void main(String[] args) {
+		Operator or = new Operator();
+		//or.operatorTest();
+		//or.operatorTest2();
+		or.operatorTest3();
+
+	}
+
+}
